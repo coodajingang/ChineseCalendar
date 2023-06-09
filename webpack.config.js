1b6792a986182ccc61d69e5c13a43941995b66bc
coodajingang <coodajingang@outlook.com>
2023-06-09 16:50:22 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    library: 'chinesecalendar',
+    libraryTarget: 'umd'
   },
   module: {
     rules: [
